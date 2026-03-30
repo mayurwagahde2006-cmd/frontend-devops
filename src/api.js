44e0d82fcbd183b1a0b72cf2121e4a14baf0f84c
Shildeep111207 <shildeepwaghmare25@gmail.com>
2026-03-30 11:02:04 +0530

    minor change

--- src/api.js
+++ src/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080', 
+//   withCredentials: true, // send cookies
+// });
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080', 
+  baseURL: import.meta.env.VITE_API_URL || 'https://dev-ops-assistant-1.onrender.com', 
   withCredentials: true, // send cookies
 });
 
