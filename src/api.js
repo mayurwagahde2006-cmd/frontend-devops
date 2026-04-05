4d3d2823f775ab859114ae620ade24ec418a789c
Mayur Waghade <mayurwaghade672@gmail.com>
2026-04-05 09:56:31 +0530

    some changes

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL || 'https://dev-ops-assistant-1.onrender.com',
-//   withCredentials: true, // ✅ necessary for cookies
-// });
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_URL || 'https://dev-ops-assistant-1.onrender.com',
   withCredentials: true, // ✅ necessary for cookies
 });
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+//   withCredentials: true, // ✅ necessary for cookies
+// });
 
 export default api;
